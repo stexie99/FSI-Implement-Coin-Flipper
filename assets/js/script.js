c2a0feca9986f2e0740e95bab53756820d430ea4
Steven Xie <xies1999@hotmail.com>
2023-03-29 15:48:11 -0400

    finished coinflip game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,50 @@
 // TODO: Declare any global variables we need
-
+let heads = 0
+let headsPercent = 0
+let tails = 0
+let tailsPercent = 0
+let coinFlip
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
     // TODO: Add event listener and handler for flip and clear buttons
+    document.querySelector('#flip').addEventListener('click', function(){
+        let coinFlip = Math.round((Math.random()))
+        //console.log(coinFlip)
+        if (coinFlip == 0){
+            document.querySelector('#pennyImg').src = '/assets/images/penny-heads.jpg'
+            heads++
+            document.querySelector('#message').textContent='You flipped heads!'
+            document.querySelector('#heads').textContent= heads} else {
+            tails ++
+            document.querySelector('#pennyImg').src = '/assets/images/penny-tails.jpg'
+            document.querySelector('#tails').textContent= tails
+            document.querySelector('#message').textContent='You flipped tails' 
+            }
+        //console.log(heads)
+        //console.log(tails)
+        let totalFlips = heads + tails
+        if (heads === 0){
+            headsPercent = 0;} else {headsPercent = Math.round(heads/totalFlips*100)}
+        if (tails === 0){
+            tailsPercent = 0;}else{tailsPercent = Math.round(tails/totalFlips*100)}
+        document.querySelector('#heads-percent').textContent=`${headsPercent}%`
+        document.querySelector('#tails-percent').textContent=`${tailsPercent}%`
+    })
+    //console.log(coinFlip)
+    document.querySelector('#clear').addEventListener('click', function(){
+        heads = 0
+        tails = 0
+        document.querySelector('#pennyImg').src = '/assets/images/penny-heads.jpg'
+        document.querySelector('#heads-percent').textContent='0%'
+        document.querySelector('#tails-percent').textContent='0%'
+        document.querySelector('#heads').textContent='0'
+        document.querySelector('#tails').textContent='0'
+        document.querySelector('#message')="Let's get rolling"
 
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
 
 
+    })
+    
     // Clear Button Click Handler
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
